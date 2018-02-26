a2aa368a11fe72149fea5e55d86d755704884ab7
Anton-Koniouchevsky <anton.koniouchevsky@gmail.com>
2018-02-26 23:38:04 +0300

    1 from 17

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  let count = 0;
+  let mask = s.split('');
+  let baseNumber = 1;
+  for(let idx = 0; idx < pairs.length; idx++) {
+    baseNumber *= pairs[idx][0];
+  }
+  //console.log(baseNumber)
+  for(let candidate = 1; candidate <= baseNumber; candidate++) {
+    mask.every((value, index) => {
+      let notDivisible = + pairs.every(([base, power]) => (candidate + index) % base != 0);
+      return value == notDivisible;
+    }) && count++;
+  }
+  return count * pairs.reduce((init, [base, pow]) => init * Math.pow(base, pow - 1), 1);
 }
